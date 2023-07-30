11f1e050a1aa1b7074e216299fb149314eeff0aa
Joao Ghabriel <joaoghabriel02@gmail.com>
2023-07-30 18:35:09 -0300

    changed localstorage code

--- src/store/cart/cart-async.ts
+++ src/store/cart/cart-async.ts
@@ -4,8 +4,8 @@ import { AppDispatch } from "..";
 
 export const getCartData = () => {
   return (dispatch: AppDispatch) => {
-    const localStorageData = JSON.parse(localStorage.getItem("cart")) || [];
-
+    let localStorageData = JSON.parse(localStorage.getItem("cart")!);
+    if (!localStorageData) localStorageData = [];
     dispatch(setInitialCart(localStorageData));
   };
 };
